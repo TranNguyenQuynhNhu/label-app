8036e71ffe0f3cf842bb6dbf7cbb28014276e8a5
Tran Nguyen Quynh Nhu <trannguyenquynhnhu24@gmail.com>
2026-05-17 14:20:35 +0700

    Conflict cases

---
+++ conflict.py
@@ -0,0 +1,41 @@
+import json
+
+# Đọc 2 file annotation
+with open("Khang_200_labels_SEA-EXAM.json", "r", encoding="utf-8") as f:
+    data1 = json.load(f)
+
+with open("Ngoc_Cleaned_0_199.json", "r", encoding="utf-8") as f:
+    data2 = json.load(f)
+
+# Map sample_id -> sample
+map1 = {sample["sample_id"]: sample for sample in data1}
+map2 = {sample["sample_id"]: sample for sample in data2}
+
+conflicts = []
+
+# Tìm các sample có conflict
+for sample_id in map1:
+    if sample_id in map2:
+        label1 = map1[sample_id]["final_label"]
+        label2 = map2[sample_id]["final_label"]
+
+        if label1 != label2:
+            conflicts.append({
+                "sample_id": sample_id,
+                "question": map1[sample_id]["question"],
+
+                "annotator_1": map1[sample_id]["annotator"],
+                "label_1": label1,
+
+                "annotator_2": map2[sample_id]["annotator"],
+                "label_2": label2,
+
+                "sample_1": map1[sample_id],
+                "sample_2": map2[sample_id]
+            })
+
+# Lưu kết quả conflict
+with open("conflicts.json", "w", encoding="utf-8") as f:
+    json.dump(conflicts, f, ensure_ascii=False, indent=2)
+
+print(f"Found {len(conflicts)} conflict samples.")
