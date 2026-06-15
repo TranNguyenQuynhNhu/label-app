d56d0c98baf77bc2aecf80ed1ade24a900c92b9d
Tran Nguyen Quynh Nhu <trannguyenquynhnhu24@gmail.com>
2026-06-15 16:25:12 +0700

    globalmmlu

--- conflict.py
+++ conflict.py
@@ -1,10 +1,10 @@
 import json
 
 # Đọc 2 file annotation
-with open("Khang_500_labels_VMLU.json", "r", encoding="utf-8") as f:
+with open("Khang_500_labels_GlobalMMLU.json", "r", encoding="utf-8") as f:
     data1 = json.load(f)
 
-with open("Nhu_500_labels.json", "r", encoding="utf-8") as f:
+with open("ANHNGOC_500_labels.json", "r", encoding="utf-8") as f:
     data2 = json.load(f)
 
 # Map sample_id -> sample
